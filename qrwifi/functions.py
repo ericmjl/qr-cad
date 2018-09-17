8ece17c3d71cea44ec98c4a1578694fd6ffb2aa0
Eric Ma <ericmajinglong@gmail.com>
2018-09-17 19:32:51 -0400

    added arr2scad function

--- qrwifi/functions.py
+++ qrwifi/functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pyqrcode as pq
+from solid import color, cube, scad_render, translate, union
 
 
 def wifi_qr(ssid: str, security: str, password: str):
@@ -26,3 +27,21 @@ def png_b64(qr, scale: int = 10):
     Return the base64 encoded PNG of the QR code.
     """
     return qr.png_data_uri(scale=scale)
+
+
+def arr2scad(arr, path):
+    """
+    Convert an array `arr` into valid OpenSCAD text.
+    """
+
+    SCALE = 2  # output defaults to 1 mm per unit; this lets us increase the size of objects proportionally.
+    cubes = [translate([i*SCALE, j*SCALE, 0])(color('black')(cube(size=[SCALE, SCALE, HEIGHT])))
+             for i, row in enumerate(arr)
+             for j, col in enumerate(row)
+             if arr[i, j] == 1]
+
+    base_plate = color('white')(cube(size=(arr.shape[0] * SCALE, arr.shape[1] * SCALE, HEIGHT / 2)))
+    qrobj = union()(*cubes, base_plate)
+
+    with open(path, 'w+') as f:
+        f.write(scad_render(qrobj))
